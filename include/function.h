e2db9795153efb1118a0e91fc1c4a28a9fc09e2a
Gary Gan <gan.fang.yi@foxmail.com>
2024-02-06 18:50:06 +0800

    初始化提交

---
+++ include/function.h
@@ -0,0 +1,9 @@
+/******************************************************************
+* Copyright (C) 2024 Gary Gan All Rights Reserverd.
+* @File Name: function.h
+* @Author: Gary Gan <gan.fang.yi@foxmail.com>
+* @Create Date: 2024-02-06
+* @Description: (I don't know how to describe it or don't wanna do)
+*******************************************************************/
+
+void my_function();
---
+++ src/main.c
@@ -0,0 +1,15 @@
+/******************************************************************
+* Copyright (C) 2024 Gary Gan All Rights Reserverd.
+* @File Name: main.c
+* @Author: Gary Gan <gan.fang.yi@foxmail.com>
+* @Create Date: 2024-02-06
+* @Description: (I don't know how to describe it or don't wanna do)
+*******************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
