223b8b99897e56a9450b05c925bb7ff1205451cc
Gary Gan <gan.fang.yi@foxmail.com>
2024-02-26 15:11:31 +0800

    add cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,9 @@
 /******************************************************************
 * Copyright (C) 2024 Gary Gan All Rights Reserverd.
-* @File Name: main.c
+* @File Name: main.cpp
 * @Author: Gary Gan <gan.fang.yi@foxmail.com>
-* @Create Date: 2024-02-06
+* @Create Date: 2024-02-26
 * @Description: (I don't know how to describe it or don't wanna do)
 *******************************************************************/
 
-#include <stdio.h>
-
-int main()
-{
-    int sum = 0;
-    for (int i = 0; i < 101; i++)
-    {
-        sum += i;
-    }
-    printf("%d\n", sum);
-    return 0;
-}
+#include <vector>
