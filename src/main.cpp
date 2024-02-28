970fbc27fd97b06fb27b01a0274c03e45f248926
Gary Gan <gan.fang.yi@foxmail.com>
2024-02-28 21:26:20 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -6,4 +6,26 @@
 * @Description: (I don't know how to describe it or don't wanna do)
 *******************************************************************/
 
-#include <vector>
+#include <iostream>
+
+template<class T>
+class CMath
+{
+public:
+    CMath(T const& t1, T const& t2):m_t1(t1), m_t2(t2) {}
+    T add();
+private:
+    T m_t1;
+    T m_t2;
+};
+
+template<class T> 
+T CMath<T>::add()
+{
+    return m_t1 + m_t2;
+}
+
+int main()
+{
+    return 0;
+}
